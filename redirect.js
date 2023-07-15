c50decee9c989c902b5b6b2edceb3dcfc4efb654
StaticVoid <100595244+Public-StaticVoid@users.noreply.github.com>
2023-07-15 17:48:08 +0200

    today we ask the age old question, can we get much higher? lets talk about it.

--- redirect.js
+++ redirect.js
@@ -1,1 +1,10 @@
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+sleep(100)
 window.location.replace("https://www.youtube.com");
