d60f99090814abae767a7390e1f131721bd47aeb
StaticVoid <100595244+Public-StaticVoid@users.noreply.github.com>
2023-09-21 06:27:06 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,2 +1,2 @@
 
-window.location.replace("https://youtube.com/playlist?list=PL1yfHUUTmgwZK_Oj8JEDdA43lQ-X-8OvJ&si=AjAEG3FiY54stlw3");
+window.location.replace("https://youtube.com/playlist?list=PL1yfHUUTmgwZK_Oj8JEDdA43lQ-X-8OvJ&si=AjAEG3FiY54stlw3")
