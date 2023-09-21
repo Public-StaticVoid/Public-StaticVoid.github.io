1bba137b9a227b98bbc01066e183d9ab405d21c5
StaticVoid <100595244+Public-StaticVoid@users.noreply.github.com>
2023-09-21 05:56:24 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,10 +1,2 @@
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
 
-sleep(99999999999999999)
 window.location.replace("https://www.youtube.com");
